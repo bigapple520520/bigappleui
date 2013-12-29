5c691d5ea36a299157230c988cc0f6f988e10e83
bigapple520520 <xuan@xuantekiMacBook-Pro.local>
2013-12-29 17:39:01 +0800

    20131229

--- trunk/com.dazzle.bigappleui/src/com/dazzle/bigappleui/viewpager/event/ScrollEventAdapter.java
+++ trunk/com.dazzle.bigappleui/src/com/dazzle/bigappleui/viewpager/event/ScrollEventAdapter.java
@@ -1,6 +1,5 @@
 package com.dazzle.bigappleui.viewpager.event;
 
-import java.util.LinkedList;
 
 /**
  * 滚动时，屏幕发生变化事件，有点像设计模式中的：订阅者模式或者观察者模式。这个是被观察者，事件改变时负责通知观察者。
@@ -9,30 +8,30 @@ import java.util.LinkedList;
  * @version $Revision: 1.0 $, $Date: 2013-7-12 下午8:17:10 $
  */
 public class ScrollEventAdapter {
-    LinkedList<OnScrollCompleteListener> onScrollCompleteListenerList;
+	LinkedList<OnScrollCompleteListener> onScrollCompleteListenerList;
 
-    public ScrollEventAdapter() {
-        onScrollCompleteListenerList = new LinkedList<OnScrollCompleteListener>();
-    }
+	public ScrollEventAdapter() {
+		onScrollCompleteListenerList = new LinkedList<OnScrollCompleteListener>();
+	}
 
-    /**
-     * 通知所有事件
-     * 
-     * @param scrollEvent
-     */
-    public void notifyEvent(ScrollEvent scrollEvent) {
-        for (OnScrollCompleteListener onScrollCompleteListener : onScrollCompleteListenerList) {
-            onScrollCompleteListener.onScrollComplete(scrollEvent);
-        }
-    }
+	/**
+	 * 通知所有事件
+	 * 
+	 * @param scrollEvent
+	 */
+	public void notifyEvent(ScrollEvent scrollEvent) {
+		for (OnScrollCompleteListener onScrollCompleteListener : onScrollCompleteListenerList) {
+			onScrollCompleteListener.onScrollComplete(scrollEvent);
+		}
+	}
 
-    /**
-     * 添加事件到事件列表
-     * 
-     * @param onScrollCompleteListener
-     */
-    public void addListener(OnScrollCompleteListener onScrollCompleteListener) {
-        onScrollCompleteListenerList.add(onScrollCompleteListener);
-    }
+	/**
+	 * 添加事件到事件列表
+	 * 
+	 * @param onScrollCompleteListener
+	 */
+	public void addListener(OnScrollCompleteListener onScrollCompleteListener) {
+		onScrollCompleteListenerList.add(onScrollCompleteListener);
+	}
 
 }
